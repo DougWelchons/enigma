420e2046b2a8039c7b8f036f9577fb9fef8f6bc3
DougWelchons <doug.welchons@gmail.com>
2021-01-15 10:49:46 -0700

    Add tests for the cipher engine class

---
+++ test/cipher_engine_test.rb
@@ -0,0 +1,37 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/cipher_engine'
+
+class CipherEngineTest < Minitest::Test
+  def test_it_exists
+    engine = CipherEngine.new
+
+    assert_instance_of CipherEngine, engine
+  end
+
+  def test_it_can_generate_a_random_key
+    engine = CipherEngine.new
+    key_length = engine.key_gen.length
+
+    assert_equal 5, key_length
+    assert_instance_of String, engine.key_gen
+  end
+
+  def test_it_can_return_an_array_of_keys
+    engine = CipherEngine.new
+
+    assert_equal [1, 12, 23, 34], engine.set_keys('01234')
+  end
+
+  def test_it_can_set_an_array_of_offsets
+    engine = CipherEngine.new
+
+    assert_equal [4, 4, 4, 1], engine.set_offsets(15012021)
+  end
+
+  def test_it_car_return_a_set_of_encryption_keys
+    engine = CipherEngine.new
+
+    assert_equal [5, 16, 27, 35], engine.encryption_keys('01234', '15012021')
+  end
+end
