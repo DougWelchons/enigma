9173b58d6b9052325888c6aa8cf758b0d9146eb4
DougWelchons <doug.welchons@gmail.com>
2021-01-15 17:21:05 -0700

    Add output to screen functionality to runner files

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -2,4 +2,7 @@ require './lib/enigma'
 
 enigma = Enigma.new
 
-enigma.encryption(ARGV)
+message_info = enigma.decryption(ARGV)
+
+puts "Created '#{ARGV[1]}' with the key #{message_info[:key]} and date #{message_info[:date]}"
+puts "REMOVE BEFORE FINAL!!!!! #{message_info[:decryption]}"
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,4 +2,7 @@ require './lib/enigma'
 
 enigma = Enigma.new
 
-enigma.decryption(ARGV)
+message_info = enigma.encryption(ARGV)
+
+puts "Created '#{ARGV[1]}' with the key #{message_info[:key]} and date #{message_info[:date]}"
+puts "REMOVE BEFORE FINAL!!!!! #{message_info[:encryption]}"
